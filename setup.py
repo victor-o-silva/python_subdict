53881e58e85beff7fb15dee6a5aea80894e0a7ef
Victor Silva <victor_o_silva@hotmail.com>
2015-07-21 16:22:18 -0300

    preparing release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE.txt', 'r') as license_file:
 setup(
     name='subdict',
     packages=['subdict'],
-    version='0.3',
+    version='0.4',
     description='Extract subdicts from python dicts by just specifying '
                 'which keys are needed, in a dotted-syntax.',
     long_description=readme_txt,
@@ -17,5 +17,5 @@ setup(
     author_email='victor_o_silva@hotmail.com',
     license=license_txt,
     url='https://github.com/victor-o-silva/python_subdict',
-    download_url='https://github.com/victor-o-silva/python_subdict/tarball/0.3'
+    download_url='https://github.com/victor-o-silva/python_subdict/tarball/0.4'
 )
